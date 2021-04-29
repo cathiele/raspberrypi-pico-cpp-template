b570f74d12dce9aeb27d27cb1247ccba08fe5080
Carsten Thiele <software@carsten-thiele.de>
2021-04-29 22:48:34 +0200

    feature: added example call to assembler function

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 
 #define ONBOARD_LED 25
 
+extern "C" int add(int a, int b);
+
 int main()
 {
     stdio_init_all();
-    printf("Simple C/CPP Template\n");
+    printf("Simple C/CPP Template (%d + %d = %d)\n", 10, 20, add(10,20));
     gpio_init(ONBOARD_LED);
     gpio_set_dir(ONBOARD_LED, GPIO_OUT);
+    
     while (1)
     {
         gpio_put(ONBOARD_LED, 0);
